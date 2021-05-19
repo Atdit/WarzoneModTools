78ec63ec149c1252582b29617dcc530226c8acad
AtditC <ardit@atdit.ch>
2021-05-19 17:01:40 +0200

    Bump Version

--- src/main/java/ch/atdit/warzonemodtools/WMT.java
+++ src/main/java/ch/atdit/warzonemodtools/WMT.java
@@ -36,7 +36,7 @@ import static ch.atdit.warzonemodtools.util.Common.*;
 public class WMT {
     /* General Mod Variables */
     public static final String MODID = "wzmt";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String configFolder = "config/warzonemodtools";
     private static final long buildVersion = 1;
 
